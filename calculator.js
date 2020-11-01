a8abb6411cfb7280fb8bc07e0a9eeb0191d31671
Neil Ricci <iccir919@gmail.com>
2020-11-01 10:23:36 -0800

    Reset calculator project

--- calculator.js
+++ calculator.js
@@ -1,47 +1,40 @@
 'use strict';
 
+function Display(props) {
+  return (
+    <div>
+      <h3>Display: {props.display}</h3>
+    </div>
+  );
+}
+
+class Buttons extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Buttons will go here.</h3>
+      </div>
+    );
+  }
+}
+
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: "0",
-      history: ""
+      display: "0"
     }
-
-    this.handleButtonClick = this.handleButtonClick.bind(this);
-  }
-
-  handleButtonClick(e) {
-    console.log(e)
   }
 
   render() {
     return (
       <div className="calculator">
-        <div id="display">
-          {this.state.display}
-        </div>
-
-        <div className="buttons">
-            <button id="clear">AC</button>
-            <button id="divide">/</button>
-            <button id="multiply">x</button>
-            <button id="seven">7</button>
-            <button id="eight">8</button>
-            <button id="nine">9</button>
-            <button id="subtract">-</button>
-            <button id="four">4</button>
-            <button id="five">5</button>
-            <button id="six">6</button>
-            <button id="add">+</button>
-            <button id="one">1</button>
-            <button id="two">2</button>
-            <button id="three">3</button>
-            <button id="equals">=</button>
-            <button id="zero">0</button>
-            <button id="decimal">.</button>
-        </div>
-        
+        <Display display={this.state.display} />
+        <Buttons />
       </div>
     )
   }
